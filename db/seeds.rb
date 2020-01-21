72ba228fa6be684bee2c588078ddf16170b68b93
James Trinder <jamesanthonyt@live.co.uk>
2020-01-21 20:33:11 +0000

    Created routes for resturants and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Restaurant.destroy_all
+category = %w[Chinese Italian Japanese French Belgian]
+
+5.times{Restaurant.create(
+  name: Faker::Restaurant.name,
+  address: Faker::Address.street_name,
+  phone_number: Faker::PhoneNumber.phone_number,
+  category: category.sample
+)}
